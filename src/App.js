465ca1e6e5f5df1662e86826301ebb63e06a9240
farzac <zaccantte@gmail.com>
2019-10-15 00:00:21 -0300

    version1 of product-type register

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Simple_excel_export_01 from './Simple_excel_export_01'
+
+function App() {
+  console.log('passo1')
+  return (
+    
+    <div className="App">
+      
+      <Simple_excel_export_01 />
+      
+    </div>
+  );
+}
+
+export default App;
